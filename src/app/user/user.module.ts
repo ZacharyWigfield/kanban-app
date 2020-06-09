042501b62b0c3291e2e9613582dadccc775504cd
Zachary Wigfield <zewigfield@gmail.com>
2020-06-09 00:38:26 -0400

    added a home page message

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { UserRoutingModule } from './user-routing.module';
+
+// Feature modules and components
 import { SharedModule } from '../shared/shared.module';
 import { GoogleSigninDirective } from './google-signin.directive';
 import { LoginPageComponent } from './login-page/login-page.component';
