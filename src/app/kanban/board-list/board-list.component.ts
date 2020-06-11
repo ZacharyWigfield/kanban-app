4520350233cdacee86857c0c84089e50e881a8ce
Zachary Wigfield <zewigfield@gmail.com>
2020-06-11 00:19:21 -0400

    stylized the boards, the board lists, and the dialogs. Filled out the task dialogs, added buttons to the boards and board lists to open the dialogs.

--- src/app/kanban/board-list/board-list.component.ts
+++ src/app/kanban/board-list/board-list.component.ts
@@ -4,6 +4,10 @@ import { Subscription } from 'rxjs';
 import { Board } from '../board.model';
 import { BoardService } from '../board.service';
 
+// Angular Material Components
+import { MatDialog } from '@angular/material/dialog';
+import { BoardDialogComponent } from '../dialogs/board-dialog.component';
+
 @Component({
   selector: 'app-board-list',
   templateUrl: './board-list.component.html',
@@ -13,7 +17,7 @@ export class BoardListComponent implements OnInit, OnDestroy {
   boards: Board[];
   sub: Subscription;
 
-  constructor(public boardService: BoardService) {}
+  constructor(public boardService: BoardService, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.sub = this.boardService
@@ -28,6 +32,22 @@ export class BoardListComponent implements OnInit, OnDestroy {
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.boards, event.previousIndex, event.currentIndex);
     this.boardService.sortBoards(this.boards);
+  }
+
+  openBoardDialog(): void {
+    const dialogRef = this.dialog.open(BoardDialogComponent, {
+      width: '400px',
+      data: {  }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.boardService.createBoard({
+          title: result,
+          priority: this.boards.length
+        });
+      }
+    });
   }
 
 }
