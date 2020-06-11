fb66040c8560eb0f1c5da6407f85672fe203cb18
Zachary Wigfield <zewigfield@gmail.com>
2020-06-10 23:42:14 -0400

    created board and task dialog to create new boards and tasks. Also modified styling of the draggable boards and tasks on their respective components.

---
+++ src/app/kanban/dialogs/task-dialog.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-dialog',
+  template: `
+    <p>
+      task-dialog works!
+    </p>
+  `,
+  styles: []
+})
+export class TaskDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
